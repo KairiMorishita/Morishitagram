21d03c84fc6eabeec6ebe94bbab82ee902b38bf0
Kairi Morishita <k.moricchifc73@gmail.com>
2019-12-07 13:33:16 +0000

    Make the titles common

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "title", "Morishitagram"
   end
 
 end

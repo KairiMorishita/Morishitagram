20cf15baadf18ecc56f746b05e6eab72d18d6278
Kairi Morishita <k.moricchifc73@gmail.com>
2019-12-10 12:32:24 +0000

    Implement  Facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      #user.email = auth.info.email
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 

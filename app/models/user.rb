5b8c147a36be49a1a6b7f911a7dbeee6ed0d94dc
Kairi Morishita <k.moricchifc73@gmail.com>
2019-12-22 06:46:45 +0000

    Create post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many  :posts, dependent: :destroy
   validates :username, presence: true, length: { minimum: 3, maximum: 20 }
   validates :name, presence: true
   has_secure_password
